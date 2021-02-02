765f3f5cac76d0bd6636abd353a2e6e18d06263f
wlcdbb <34713574+wlcdbb@users.noreply.github.com>
2021-02-02 16:51:44 +0800

    1.1
    更新图片

--- src/project.js
+++ src/project.js
@@ -987,9 +987,9 @@ window.__require = function e(t, n, o) {
                         scale: .9
                     }).union().repeatForever().start()
                 }, t.prototype.update = function(e) {}, t.prototype.adsButtonFunc2 = function() {
-                    window.location.href = "https://www.qwas.top"
+                    window.location.href = "https://www.roewe.com.cn"
                 }, t.prototype.bannerButtonFunc = function() {
-                    window.location.href = "https://www.qwas.top"
+                    window.location.href = "https://www.roewe.com.cn"
                 }, t.prototype.ShowFailedUi = function(e, t) {
                     e = e * (window.difficulty[0] + 1);
                     t = t * (window.difficulty[0] + 1);
@@ -1826,7 +1826,7 @@ window.__require = function e(t, n, o) {
                 cc.find("Canvas/adsLayer").active = !0, cc.find("Canvas/adsLayer").setContentSize(cc.winSize.width, cc.winSize.height)
             },
             AdsLink: function() {
-                window.location.href = "https://www.qwas.top"
+                window.location.href = "https://www.roewe.com.cn"
             },
             closeAdsLayer: function() {
                 cc.find("Canvas/adsLayer").active = !1
@@ -1917,7 +1917,7 @@ window.__require = function e(t, n, o) {
                 }, t.prototype.update = function(e) {
                     this.UpdateScoreLabel(e), this.lerpCtrl && this.lerpNumFunc(this.passlevelYQ), this.levelPanel.children[1].getComponent(cc.Label).string = s.default.Instance.GetLevel().toString()
                 }, t.prototype.adsButtonFunc = function() {
-                    window.location.href = "https://www.qwas.top"
+                    window.location.href = "https://www.roewe.com.cn"
                 }, t.prototype.TestPasslevel = function() {
                     var e = this;
                     this.lerpCtrl = !0, this.nowYQ >= this.passlevelYQ && (this.levelPanel.children[2].runAction(cc.sequence(cc.delayTime(1.3), cc.callFunc(function() {
